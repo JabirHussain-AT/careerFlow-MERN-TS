8e539467e6e0bef20a24fca345fb69f11849fff9
Jabirrr <jabirmhd6786@gmail.com>
2024-03-15 09:44:53 +0530

    bug fixing

--- services/company-service/src/util/cronJob/forCheckingExpiry.ts
+++ services/company-service/src/util/cronJob/forCheckingExpiry.ts
@@ -4,13 +4,13 @@ import Jobs from "../../adapters/database/schemas/jobSchema";
 const updateExpiredJobs = async () => {
   try {
     const currentDate = new Date();
-    const expiredJobs = await Jobs.find({ jobExpiry: { $lt: currentDate } });
+    const expiredJobs = await Jobs.find({ jobExpiry: { $lt: currentDate } , status: true });
     if (expiredJobs.length > 0) {
       const updatePromises = expiredJobs.map(async (job: any) => {
         await Jobs.findByIdAndUpdate(job._id, { status: false });
       });
       await Promise.all(updatePromises);
-      console.log("Expired jobs updated successfully.");
+      console.log("Expired jobs updated successfully.",expiredJobs);
     } else {
       console.log("No expired jobs found.");
     }
