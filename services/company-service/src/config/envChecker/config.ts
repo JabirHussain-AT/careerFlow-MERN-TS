9ecdde2760fc533b876ce76c768926497d48fdc4
Jabirrr <jabirmhd6786@gmail.com>
2024-03-29 11:48:44 +0530

    jwt update

--- services/company-service/src/config/envChecker/config.ts
+++ services/company-service/src/config/envChecker/config.ts
@@ -1,7 +1,7 @@
 let EMAIL : string = process.env.EMAIL
 let PASSWORD : string = process.env.PASSWORD
 let HASH_SALT : number = 10
-let JWt_SECRET : string = process.env.JWt_SECRET
+let JWt_SECRET : string = process.env.JWT_SECRET
 let NODE_ENV : string = process.env.NODE_ENV
 
 export {
