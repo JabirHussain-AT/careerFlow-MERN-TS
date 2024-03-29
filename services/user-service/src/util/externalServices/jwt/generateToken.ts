f6835e11b193815753874c10628182b8e4ceae2f
Jabirrr <jabirmhd6786@gmail.com>
2024-03-29 12:02:44 +0530

    changeed env

--- services/user-service/src/util/externalServices/jwt/generateToken.ts
+++ services/user-service/src/util/externalServices/jwt/generateToken.ts
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken'
-import {JWt_SECRET} from '../../../config/envConfig/config'
+// import {JWt_SECRET} from '../../../config/envConfig/config'
 
 
 export const generateToken = ( payload : { _id : string , email : string , role : string  } )  => {
 
-    return jwt.sign(payload,JWt_SECRET, { expiresIn: '20d' });
+    return jwt.sign(payload,process.env.JWT_SECRET, { expiresIn: '20d' });
     
 };
