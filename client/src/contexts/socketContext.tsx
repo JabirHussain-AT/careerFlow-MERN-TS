dd5eb2eecae3536f35e42e7b7d27fe95b1315665
Jabirrr <jabirmhd6786@gmail.com>
2024-03-29 10:12:50 +0530

    chat - api update

--- client/src/contexts/socketContext.tsx
+++ client/src/contexts/socketContext.tsx
@@ -20,6 +20,7 @@ export const SocketProvider: React.FC<SocketContextProps> = ({ children }) => {
 
   useEffect(() => {
     const newSocket = io("http://localhost:3005");
+    // const newSocket = io("https://careerflow.shop/api/chat");  
     setSocket(newSocket);
 
     newSocket.on("new-message", (message: any) => {
--- services/chat-service/src/start.ts
+++ services/chat-service/src/start.ts
@@ -1,55 +1,56 @@
-import dotenv from 'dotenv'
-import http from 'http'
-dotenv.config()
-import express , {Express, Request , Response} from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import session from 'express-session'
-import { routes } from './adapters/routes'
-import dependencies from './utils/config/dependencies'
-import errHandler from './utils/errorHandlers/errorHandler'
-import connectSocketIo from './infra-socket/connection'
-import color from 'colors'
+import dotenv from "dotenv";
+import http from "http";
+dotenv.config();
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import { routes } from "./adapters/routes";
+import dependencies from "./utils/config/dependencies";
+import errHandler from "./utils/errorHandlers/errorHandler";
+import connectSocketIo from "./infra-socket/connection";
+import color from "colors";
 
+const app: Express = express();
+const PORT: number = Number(process.env.PORT);
 
+app.use(express.json());
+app.use(cookieParser());
 
-const app : Express = express()
-const PORT : number = Number(process.env.PORT) 
+app.use(
+  cors({
+    origin: process.env.FRONT_END_URL, // Replace with the actual origin of your frontend
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-
-
-app.use(cors({
-  origin: process.env.FRONT_END_URL, // Replace with the actual origin of your frontend
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  credentials: true,
-}));
-
-
-
-app.use(session({
-    secret : String(process.env.SESSION_SECRET),
-    resave : true, 
-    saveUninitialized : true
-}))
-
-
-app.use('/api/chat',routes(dependencies))
-
-app.use((req: Request, res: Response) => {
-  res.status(404).json({ success: false, status: 404, message: "Api Not found" });
+const server = http.createServer(app);
+app.use("/api/chat", routes(dependencies));
+app.use("/api/connect", (req, res) => {
+  connectSocketIo(server);
 });
 
-const server = http.createServer(app)
+app.use((req: Request, res: Response) => {
+  res
+    .status(404)
+    .json({ success: false, status: 404, message: "Api Not found" });
+});
 
 app.use(errHandler);
 
-server.listen( PORT, () => {
-  console.log(color.green(`chat service starte successfully at the port ${PORT}`));
-})
+server.listen(PORT, () => {
+  console.log(
+    color.green(`chat service started successfully at the port ${PORT}`)
+  );
+});
 
-connectSocketIo(server)
-
-export default app
+export default app;
