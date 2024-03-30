c0fa9cab98111f9c9772f8eee331706240aa2fc1
Jabirrr <jabirmhd6786@gmail.com>
2024-03-30 10:54:28 +0530

    host-fix : socket-fixing

--- client/src/contexts/socketContext.tsx
+++ client/src/contexts/socketContext.tsx
@@ -19,8 +19,9 @@ export const SocketProvider: React.FC<SocketContextProps> = ({ children }) => {
   const [messages, setMessages] = useState<any[]>([]); // State to store messages
 
   useEffect(() => {
-    const newSocket = io("http://localhost:3005");
-    // const newSocket = io("https://careerflow.shop/api/chat");  
+    // const newSocket = io("http://localhost:3005");
+    // const newSocket = io("https://web.careerflow.shop/api/chat");  
+    const newSocket = io("https://web.careerflow.shop/api/chat");  
     setSocket(newSocket);
 
     newSocket.on("new-message", (message: any) => {
